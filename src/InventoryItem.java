b1287f013c1f36ab6a3697c2bd54db1ef1cbdd5d
Jose Benitez <jbtek1986@outlook.com>
2023-03-01 22:55:49 -0800

    changes

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -4,4 +4,31 @@ public class InventoryItem {
     private int quantity;
     private double cost;
     private double totalCost;
+
+    InventoryItem(){
+        itemNumber = 0;
+        quantity = 0;
+        cost = 0.00;
+        totalCost = 0.00;
+    }
+
+    InventoryItem(int itemNum, double itemCost, int itemQty){
+        itemNumber = itemNum;
+        cost = itemCost;
+        quantity = itemQty;
+
+        totalCost = setTotalCost();
+    }
+
+    public void setItemNumber(int itemNum){
+        this.itemNumber = itemNum;
+    }
+
+    public void setQuantity(int itemQty){
+        this.quantity = itemQty;
+    }
+
+    public void setCost(){
+
+    }
 }
